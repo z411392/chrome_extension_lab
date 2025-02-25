ceee81df07ef5d0ca54e82a80901d66ef0bf8e32
z411392 <z411392@gmail.com>
2025-02-25 10:47:58 +0800

    feat: 嘗試在 content_scripts 裡引用 jQuery（方便日後作 DOM Manipulation）

--- contents/https/all/all/js/main.js
+++ contents/https/all/all/js/main.js
@@ -1,4 +1,3 @@
-const span = document.createElement("span")
-span.textContent = "here we go"
-span.style.cssText = "color: red; font-size: 20px; font-weight: bold; margin: 10px;"
-document.body.appendChild(span)
+$(document).ready(() => {
+    console.log('jQuery loaded')
+})
