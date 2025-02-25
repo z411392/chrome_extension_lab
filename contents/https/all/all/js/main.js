cf402ae6b1014eaa5da7021b5ba7cf4d8e2a15e6
z411392 <z411392@gmail.com>
2025-02-25 10:43:24 +0800

    feat: 嘗試注入 html

--- contents/https/all/all/js/main.js
+++ contents/https/all/all/js/main.js
@@ -1,1 +1,4 @@
-console.log("hello world")
+const span = document.createElement("span")
+span.textContent = "here we go"
+span.style.cssText = "color: red; font-size: 20px; font-weight: bold; margin: 10px;"
+document.body.appendChild(span)
